8ce1dbdbf288ff348b3112eb950d4276610e0a46
Possidesiree <desipossi01@gmail.com>
2023-05-14 13:02:20 +0200

    Import the dataSet into the project

---
+++ dataImport.py
@@ -0,0 +1,10 @@
+# This is a comment
+
+# Importing necessary libraries
+import pandas as pd
+
+# Reading in data from a CSV file
+data = pd.read_csv('Stress.csv')
+
+# Printing out the first 5 rows of the data
+print(data.head())
