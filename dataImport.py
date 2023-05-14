276b1b90850ed8ddf503086fbca71c30ea34102b
Possidesiree <desipossi01@gmail.com>
2023-05-14 20:23:56 +0200

    scrypt for familiarisation with the dataset

--- dataImport.py
+++ dataImport.py
@@ -2,9 +2,11 @@
 
 # Importing necessary libraries
 import pandas as pd
+import numpy as np
 
 # Reading in data from a CSV file
 data = pd.read_csv('Stress.csv')
 
 # Printing out the first 5 rows of the data
 print(data.head())
+data.head()
